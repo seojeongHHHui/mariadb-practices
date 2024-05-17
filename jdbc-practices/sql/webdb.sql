51a566126a25bbc89200bec08755ba9ffad95440
seojeongHHHui <sjhui0523@gmail.com>
2024-05-17 14:48:51 +0900

    jdbc #2

---
+++ jdbc-practices/sql/webdb.sql
@@ -0,0 +1,13 @@
+-- insert
+insert into dept values(null, '디자인팀');
+
+-- delete
+delete from dept where no = 7;
+
+-- update
+update dept set name='시스템개발' where no = 2;
+
+-- select
+select * from dept;
+
+select * from emp;
