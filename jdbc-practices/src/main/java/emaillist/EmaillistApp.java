219a569e68b546e7471c410a2612d9bd15cf7a57
seojeongHHHui <sjhui0523@gmail.com>
2024-05-17 15:51:01 +0900

    emaillist #1

---
+++ jdbc-practices/src/main/java/emaillist/EmaillistApp.java
@@ -0,0 +1,47 @@
+package emaillist;
+
+import java.util.Scanner;
+
+public class EmaillistApp {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("(l)ist (d)elete (i)nsert (q)uit > ");
+			String command = scanner.nextLine();
+			
+			if("l".equals(command)) {
+				doList();
+				
+			} else if("d".equals(command)) {
+				doDelete();
+				
+			} else if("i".equals(command)) {
+				doInsert();
+				
+			} else if("q".equals(command)) {
+				break;
+			}
+			
+		}
+		
+		scanner.close();
+		
+	}
+
+	private static void doInsert() {
+		System.out.println("doInsert");
+	}
+
+	private static void doDelete() {
+		System.out.println("doDelete");
+		// email 물어보기
+		// 삭제
+	}
+
+	private static void doList() {
+		System.out.println("doList");
+	}
+
+}
---
+++ jdbc-practices/src/main/java/example/DeleteEx02.java
@@ -0,0 +1,65 @@
+package example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteEx02 {
+
+	public static void main(String[] args) {
+		boolean result = delete(8L);
+		System.out.println(result ? "성공" : "실패");
+
+	}
+	
+	private static boolean delete(long no) {
+		boolean  result = false;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			// 1. JDBC Driver 로딩
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			// 2. 연결하기
+			String url = "jdbc:mariadb://192.168.0.196:3306/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			
+			// 3. Statement 준비
+			String sql = "delete from dept where no = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. binding
+			pstmt.setLong(1, no);
+			
+			// 5. SQL 실행
+			int count = pstmt.executeUpdate();
+			
+			// 5. 결과처리
+			result = count == 1;
+			
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+}
